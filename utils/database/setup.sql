e99d981c7d815f13304be9c444cb2a1c1d4fb2ef
RichardDorian <richard.dorian@outlook.com>
2024-04-04 09:50:48 +0200

    add comments table

--- utils/database/setup.sql
+++ utils/database/setup.sql
@@ -21,3 +21,16 @@ CREATE TABLE `atlashardware`.`posts` (
   FOREIGN KEY (`author`) REFERENCES `users`(`id`),
   FOREIGN KEY (`forked_from`) REFERENCES `posts`(`id`)
 ) ENGINE = InnoDB;
+
+CREATE TABLE `atlashardware`.`comments` (
+  `id` BINARY(16) NOT NULL,
+  `author` BINARY(16) NOT NULL,
+  `post` BINARY(16) NOT NULL,
+  `replied_to` BINARY(16) NULL DEFAULT NULL,
+  `date` DATETIME NOT NULL,
+  `comment` TEXT NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`author`) REFERENCES `users`(`id`),
+  FOREIGN KEY (`post`) REFERENCES `posts`(`id`),
+  FOREIGN KEY (`replied_to`) REFERENCES `comments`(`id`)
+) ENGINE = InnoDB;

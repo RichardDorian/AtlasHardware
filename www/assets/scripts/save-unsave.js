fc528f4b86806ebcbd00a7b5877441a17c76e3e0
RichardDorian <richard.dorian@outlook.com>
2024-04-11 20:32:53 +0200

    add scope to save-unsave script

--- www/assets/scripts/save-unsave.js
+++ www/assets/scripts/save-unsave.js
@@ -1,19 +1,21 @@
-const saveUnsaveElements = document.querySelectorAll('[data-save-unsave]');
+{
+  const saveUnsaveElements = document.querySelectorAll('[data-save-unsave]');
 
-for (const element of saveUnsaveElements) {
-  element.addEventListener('click', async () => {
-    const id = element.dataset.saveUnsave;
-    const saved = element.classList.contains('added');
-    const route = saved ? '/me/unsave' : '/me/save';
+  for (const element of saveUnsaveElements) {
+    element.addEventListener('click', async () => {
+      const id = element.dataset.saveUnsave;
+      const saved = element.classList.contains('added');
+      const route = saved ? '/me/unsave' : '/me/save';
 
-    await fetch(route, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({ post: id }),
+      await fetch(route, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({ post: id }),
+      });
+
+      location.reload();
     });
-
-    location.reload();
-  });
+  }
 }

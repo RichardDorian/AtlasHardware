56391be54776abd9b58cc8435b1714d4e0449da9
Ashokas <99681959+Ashokaas@users.noreply.github.com>
2024-04-15 18:45:24 +0200

    Responsive nav bar

--- components/header/header.php
+++ components/header/header.php
@@ -1,8 +1,16 @@
 <?php include_once __DIR__ . "/../../utils/user_session.php" ?>
 
 <header>
-  <a href="/">Home</a>
-  <a href="/browse">Browse</a>
+  <a href="/" class="with-icon">
+    <span class="material-symbols-rounded">home</span>
+    <span>Home</span>
+  </a>
+  
+  <a href="/browse" class="with-icon">
+    <span class="material-symbols-rounded">travel_explore</span>
+    <span>Browse</span>
+  </a>
+  
   <form action="/search" method="get" id="search">
     <input type="text" name="query" id="query" placeholder="Search Atlas Hardware" required>
     <button type="submit">

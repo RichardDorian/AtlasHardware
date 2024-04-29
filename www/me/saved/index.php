a0de3daa5680354c311b8442e8bfc4df36ee7ca7
Ashokas <99681959+Ashokaas@users.noreply.github.com>
2024-04-29 23:16:50 +0200

    Add saved posts

---
+++ www/me/saved/index.php
@@ -0,0 +1,42 @@
+<?php include_once __DIR__ . "../../../../components/cards/index.php" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$styles = ["header.css", "cards.css", "saved.css", "footer.css"];
+$scripts = ["header.js", "save-unsave.js"];
+$title = "Saved posts";
+include_once __DIR__ . "../../../../components/head/head.php";
+?>
+
+<body>
+  <?php include_once __DIR__ . "../../../../components/header/header.php" ?>
+  <main>
+    <?php
+    include_once __DIR__ . "../../../../utils/posts.php";
+    if (UserSession::is_connected()) {
+      $saved_builds = UserSession::get_saved_posts();
+    }
+    ?>
+    <h1>
+    <span class="material-symbols-rounded">bookmark</span>
+      <span>Saved Builds</span>
+    </h1>
+    <div>
+      <div class="section-content">
+        <?php
+        if (count($saved_builds) === 0) {
+          echo "<p>You haven't saved any build yet</p>";
+        }
+        foreach ($saved_builds as $post) {
+          small_card($post, true);
+        }
+        ?>
+      </div>
+    </div>
+
+  </main>
+  <?php include_once __DIR__ . "../../../../components/footer/footer.php" ?>
+</body>
+
+</html>

33363e08c5ee853291757267a299eb9bd45c4a43
Ashokas <99681959+Ashokaas@users.noreply.github.com>
2024-05-03 14:05:49 +0200

    Add comments 404 page

--- www/404.php
+++ www/404.php
@@ -1,23 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
+
+// Define the CSS and JS files to be included in the head of the document
 $styles = ["header.css", "404.css", "footer.css"];
 $scripts = ["header.js"];
+
 $title = "Not Found";
+
+// Include the head component with the defined variables
 include_once __DIR__ . "/../components/head/head.php";
 ?>
 
 <body>
-  <?php include_once __DIR__ . "/../components/header/header.php" ?>
 
+  <?php
+  // Include the header component
+  include_once __DIR__ . "/../components/header/header.php"
+  ?>
+
+  <!-- Main content of the page -->
   <main>
     <h1>(;-;)</h1>
     <h2>The page you are looking for doesn't exist</h2>
   </main>
 
   <?php
+  // Include the footer component
   include_once __DIR__ . "/../components/footer/footer.php"
   ?>
+
 </body>
 
 </html>
